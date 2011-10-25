c9a35345bd53547579f379c2a8e82adac23d929e
Jan Molak <jan.molak@gmail.com>
2011-10-25 08:47:00 +0100

    Replaced hard-coded item prices with a PRICE_LIST hash

--- lib/order.rb
+++ lib/order.rb
@@ -1,4 +1,4 @@
-class Order
+module Shop
   PRICE_LIST = {
     :shirt     => 5.60,
     :pants     => 3,
@@ -9,43 +9,46 @@ class Order
     :car       => 1000.00,
   }
 
-  def initialize &block
-    @products = {}
-    instance_eval(&block) if block_given?
-  end
+  class Order
 
-  def add product, quantity=1
-    self[product] = quantity
-  end
-
-  def []= product, quantity
-    if PRICE_LIST.keys.include?(product)
-      @products[product] = (@products.keys.include?(product) && (@products[product] + quantity)) || quantity
-    end
-  end
-
-  def [] product
-    @products[product] unless @products[product].nil?
-  end
-
-  def total_price product=nil
-    total = 0
-    if product.nil?
-      @products.each { |product, quantity| total += quantity * PRICE_LIST[product] }
-    else
-      total = self[product] * PRICE_LIST[product] unless self[product].nil?
+    def initialize &block
+      @products = {}
+      instance_eval(&block) if block_given?
     end
 
-    total
-  end
-
-  def print_receipt
-    @products.each do |product, quantity|
-      puts "#{"#{product} (#{quantity}x)".ljust(20, " ")} - #{"#{total_price product}".rjust(6, " ")}"
+    def add product, quantity=1
+      self[product] = quantity
     end
 
-    puts "".ljust(29, "-")
-    puts "#{"Total:".ljust(20, " ")} #{"#{total_price}".rjust(8, " ")}"
-  end
+    def []= product, quantity
+      if PRICE_LIST.keys.include?(product)
+        @products[product] = (@products.keys.include?(product) && (@products[product] + quantity)) || quantity
+      end
+    end
 
+    def [] product
+      @products[product] unless @products[product].nil?
+    end
+
+    def total_price product=nil
+      total = 0
+      if product.nil?
+        @products.each { |product, quantity| total += quantity * PRICE_LIST[product] }
+      else
+        total = self[product] * PRICE_LIST[product] unless self[product].nil?
+      end
+
+      total
+    end
+
+    def print_receipt
+      @products.each do |product, quantity|
+        puts "#{"#{product} (#{quantity}x)".ljust(20, " ")} - #{"#{total_price product}".rjust(6, " ")}"
+      end
+
+      puts "".ljust(29, "-")
+      puts "#{"Total:".ljust(20, " ")} #{"#{total_price}".rjust(8, " ")}"
+    end
+
+  end
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,65 +4,67 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/../")
 
 require 'lib/order.rb'
 
-describe Order do
-  it "should add a new product to the order" do
+module Shop
+  describe Order do
+    it "should add a new product to the order" do
 
-    order = Order.new do
-      add :shirt, 2
+      order = Order.new do
+        add :shirt, 2
+      end
+
+      order[:shirt].should == 2
     end
 
-    order[:shirt].should == 2
-  end
+    it "should default the quantity to 1, if it's not provided" do
+      order = Order.new do
+        add :shirt
+      end
 
-  it "should default the quantity to 1, if it's not provided" do
-    order = Order.new do
-      add :shirt
+      order[:shirt].should == 1
     end
 
-    order[:shirt].should == 1
-  end
+    it "should increase the quantity of a product" do
+      order = Order.new do
+        add :shirt, 2
+        add :shirt, 3
+      end
 
-  it "should increase the quantity of a product" do
-    order = Order.new do
-      add :shirt, 2
-      add :shirt, 3
+      order[:shirt].should == 5
     end
 
-    order[:shirt].should == 5
-  end
+    it "should calculate the total price" do
+      order = Order.new do
+        add :pants
+        add :jacket
+      end
 
-  it "should calculate the total price" do
-    order = Order.new do
-      add :pants
-      add :jacket
+      order.total_price.should == PRICE_LIST[:pants] + PRICE_LIST[:jacket]
     end
 
-    order.total_price.should == 3+20 # get from the PRICE_LIST
-  end
+    it "should calculate the total price of a given product on the order" do
+      order = Order.new do
+        add :pants
+        add :pants, 3
+        add :jacket
+      end
 
-  it "should calculate the total price of a given product on the order" do
-    order = Order.new do
-      add :pants
-      add :pants, 3
-      add :jacket
+      order.total_price(:pants).should == 4 * PRICE_LIST[:pants]
     end
 
-    order.total_price(:pants).should == 4 * 3 # get from the PRICE_LIST
-  end
-
-  it "should return a total price of 0 for a product that's not on the order" do
-    order = Order.new
-    order.total_price(:pants).should == 0
-  end
-
-  it "should print a receipt" do
-    order = Order.new do
-      add :pants
-      add :pants, 3
-      add :jacket
+    it "should return a total price of 0 for a product that's not on the order" do
+      order = Order.new
+      order.total_price(:pants).should == 0
     end
 
-    order.print_receipt # add a regex matcher
-  end
+    it "should print a receipt" do
+      order = Order.new do
+        add :pants
+        add :pants, 3
+        add :jacket
+      end
 
+      order.print_receipt # add a regex matcher
+    end
+
+  end
 end
